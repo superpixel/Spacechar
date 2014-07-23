c39c5df776b8903c3df09b78e6d9f9ce42b47e2a
Nico Rohrbach <nico@superpixel.ch>
2014-07-23 21:13:33 +0200

    Strings fix and added german localization

--- compile.sh
+++ compile.sh
@@ -2,4 +2,5 @@
 BASEDIR=$(dirname "$0")
 osacompile -o "$BASEDIR/Spacechar.app" "$BASEDIR/Spacechar.applescript"
 cp -R "$BASEDIR/en.lproj" "$BASEDIR/Spacechar.app/Contents/Resources/"
+cp -R "$BASEDIR/de.lproj" "$BASEDIR/Spacechar.app/Contents/Resources/"
 echo "Compiled Spacechar.app"
